cee8dd97c5f87bca42a2fcfad64fa98cf080d7e8
AtlantixJJ <xujj15@mails.tsinghua.edu.cn>
2018-07-03 21:19:32 +0800

    dataloader complete

--- project/src/dataset.py
+++ project/src/dataset.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 import tensorpack as tp
 
@@ -7,6 +8,8 @@ SLOPE_DENOTE_LEN = 40
 TLEN = 40 # LEN of average time
 THRESHOLD1 = 0.05 #0.002
 THRESHOLD2 = 0.10 #0.004
+INPUT_LEN = 200
+OUTPUT_LEN = 40
 
 def get_class(div):
     """
@@ -69,43 +72,125 @@ def get_inst_type(name):
         if INST_TYPE[i] in name:
             return i
 
-
 class RandomShuffler(tp.dataflow.RNGDataFlow):
-    def __init__(self, datasets):
-        if type(datasets) == list and len(datasets) == 1:
-            self.datasets = datasets[0]
-        else:
-            self.datasets = datasets
+    def __init__(self, datasets, shuffle=True):
+        self.shuffle = shuffle
+        self.datasets = datasets
+
+        self.len = min([len(d) for d in self.datasets])
+
+    def size(self):
+        return self.len
 
     def get_data(self):
-        idxs = np.arange(len(self.files))
+        idxs = np.arange(self.len)
         if self.shuffle:
             self.rng.shuffle(idxs)
+        
+        for idx in idxs:
+            yield [d[idx] for d in self.datasets]
+
+class DataLoader(object):
+    def __init__(self, dataset, batch_size, shuffle=True, num_threads=2):
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.dataflow = RandomShuffler(dataset, self.shuffle)
+        self.num_threads = num_threads
     
+        self.ds1 = tp.dataflow.BatchData(self.dataflow, self.batch_size)
+        self.ds2 = tp.dataflow.PrefetchData(self.ds1, nr_prefetch=32, nr_proc=self.num_threads)
+        self.ds2.reset_state()
+
+    def reset_state(self):
+        tp.logger.info("Reset dataloader")
+        self.dataflow.reset_state()
+        self.ds1.reset_state()
+        self.ds2.reset_state()
+
+    def generator(self):
+        return self.ds2.get_data()
 
 class FuturesData(object):
-    def __init__(self, data_dir, shuffle=True):
-        self.data_dir = data_dir
-        self.shuffle = shuffle
-    
-        self.__read_data()
+    def __init__(self, from_npz=False):
+        self.data_dir = "futuresData"
+
+        if from_npz:
+            self.__load_npz()
+        else:
+            self.__read_data()
+        self.__proc()
+
+    def __len__(self):
+        return self.min_length
+
+    def __save_npz(self):
+        np.savez("futuresData", [self.all_data, self.earliest_time, self.length])
+
+    def __load_npz(self):
+        npz = np.load("futuresData.npz")['arr_0']
+        self.all_data, self.earliest_time, self.length = npz
 
     def __read_data(self):
         files = os.listdir(self.data_dir)
         files.sort()
         
         self.all_data = [[], [], [], []]
+        self.earliest_time = [[], [], [], []]
+        self.length = [[], [], [], []]
         for f in files:
             if "csv" in f:
-                print(f)
                 df = pd.read_csv(os.path.join(self.data_dir, f), index_col=0, parse_dates=True)
+                df_len = len(df[df.columns[0]])
+                if df_len < 1000:
+                    continue
                 type_id = get_inst_type(f)
                 df[u'meanPrice'] = get_mean_price(df[u'bidPrice1'])
                 self.all_data[type_id].append(df[u'meanPrice'])
+                self.earliest_time[type_id].append(df[df.columns[0]][INPUT_LEN+1])
+                self.length[type_id].append(df_len)
+        
+        self.all_data = np.array(self.all_data)
+        self.earliest_time = np.array(self.earliest_time)
+        self.length = np.array(self.length)
+    
+    def __proc(self):
+        self.file_number = self.all_data.shape[1]
         
         # generate sampling time
+        self.min_length = self.length.min() - TLEN - 1
+        self.idx = np.arange(self.min_length * self.file_number)
 
-    
     def __getitem__(self, idx):
-        pass
-                
+        file_idx = idx // self.min_length
+        # retry until find a proper end time
+        tried = []
+        while True:
+            seq = []
+            # the inst_type used to mark an end
+            end_type = np.random.randint(0, 4)
+            if end_type in tried:
+                continue
+            else:
+                tried.append(end_type)
+
+            # use end inst type to find an ending at a timestamp
+            scaled_idx = int((self.length[end_type, file_idx] - INPUT_LEN - 1) / float(self.min_length) * idx)
+            barrier_time = self.all_data[end_type, file_idx].index[INPUT_LEN + scaled_idx]
+            
+            IS_FAILED = False
+            # exam other inst type
+            for inst_type in range(4):
+                # find the latest time before barrier
+                end_idx = self.all_data[inst_type, file_idx].index.get_loc(barrier_time, 'ffill')
+                if end_idx < INPUT_LEN:
+                    IS_FAILED = True
+                    break
+                seq.append(self.all_data[inst_type, file_idx][end_idx-INPUT_LEN:end_idx].as_matrix())
+            
+            # retry if failed
+            if IS_FAILED:
+                continue
+            else:
+                break
+
+        return np.array(seq)
--- project/src/test.py
+++ project/src/test.py
@@ -1,7 +1,15 @@
 import sys
 sys.path.insert(0, "src")
 import dataset
+import numpy as np
 import pandas as pd
 
-DATA_DIR = "futuresData"
-futures_data = dataset.FuturesData(DATA_DIR)
+#DATA_DIR = "futuresData"
+#futures_data = dataset.FuturesData(DATA_DIR)
+#futures_data.save_npz()
+
+dl = dataset.DataLoader([dataset.FuturesData(from_npz=True)], 64)
+for idx, sample in enumerate(dl.generator()):
+    sample = np.array(sample[0])
+    print(sample.shape)
+dl.reset_state()
